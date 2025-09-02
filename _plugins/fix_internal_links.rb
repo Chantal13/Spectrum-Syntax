5a7d0f6457bc08a39cc3ec71c95c3f058852e55a
Chantal <Chantal13@users.noreply.github.com>
2025-09-01 21:38:49 -0400

    _plugins/fix_internal_links.rb:1 Converts bad absolute section links (e.g., https://rockhounding/...) to root-absolute (/rockhounding/). Ensures any section href like "rockhounding/..." becomes "/rockhounding/". Applies to games, rockhounding, tumbling, hobbies, blog. _includes/breadcrumbs.html Uses {{ target | relative_url }} for crumbs; removes any extra leading marker after Home.

--- _plugins/fix_internal_links.rb
+++ _plugins/fix_internal_links.rb
@@ -9,10 +9,9 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   # Fix mistakenly absolute "https://<section>/..." links -> "/<section>/..."
   %w[games rockhounding tumbling hobbies blog].each do |section|
     html.gsub!(/href=(["'])https?:\/\/#{section}\//i, 'href=\1/' + section + '/')
+    # Ensure section links are root-absolute (add leading slash if missing)
+    html.gsub!(/href=(["'])#{section}\//i, 'href=\1/' + section + '/')
   end
-  # Also normalize to root-absolute
-  html.gsub!(/href=(['"])\/games\//i, 'href=\1/games/')
-  html.gsub!(/href=(['"])games\//i, 'href=\1/games/')
 
   # Ensure spaces in internal hrefs are percent-encoded
   html.gsub!(/href=(['"])(\/[^'"#?]*\s[^'"#?]*)(['"#?])/) do
